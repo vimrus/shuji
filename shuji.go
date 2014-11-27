4b59e0c519d4596cf99b5b8ccb5b1c382160505a
vimrus <vimrus.com@gmail.com>
2014-11-27 22:55:40 +0800

    * use httprouter.

--- shuji.go
+++ shuji.go
@@ -2,16 +2,18 @@ package main
 
 import (
 	"fmt"
+	"github.com/julienschmidt/httprouter"
+	"log"
 	"net/http"
 )
 
-func index(w http.ResponseWriter, r *http.Request) {
+func index(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 	fmt.Fprintf(w, "hello shuji!")
 }
 
 func main() {
-	http.HandleFunc("/", index)
-	if err := http.ListenAndServe(":8080", nil); err != nil {
-		panic(err)
-	}
+	router := httprouter.New()
+	router.GET("/", index)
+
+	log.Fatal(http.ListenAndServe(":8080", router))
 }
