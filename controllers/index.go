c41fed8128b63c1827f02339cfaf7198200b2e30
vimrus <vimrus.com@gmail.com>
2014-11-28 23:25:19 +0800

    * use pongo2 with template.

--- controllers/index.go
+++ controllers/index.go
@@ -1,21 +1,15 @@
 package controllers
 
 import (
-	"fmt"
 	"github.com/flosch/pongo2"
 	"github.com/julienschmidt/httprouter"
 	"net/http"
 )
 
 func Index(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
-	tpl, err := pongo2.FromString("Hello {{ name|capfirst }}!")
+	tpl := pongo2.Must(pongo2.FromFile("templates/index.html"))
+	err := tpl.ExecuteWriter(pongo2.Context{"name": "Shuji!"}, w)
 	if err != nil {
-		panic(err)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
 	}
-
-	out, err := tpl.Execute(pongo2.Context{"name": "shuji"})
-	if err != nil {
-		panic(err)
-	}
-	fmt.Fprintf(w, out)
 }
