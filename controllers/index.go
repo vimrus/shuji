579e0bde441066959ed87035eb6cc90a9eef3855
vimrus <vimrus.com@gmail.com>
2014-11-28 23:02:35 +0800

    + add pongo2.

--- controllers/index.go
+++ controllers/index.go
@@ -2,10 +2,20 @@ package controllers
 
 import (
 	"fmt"
+	"github.com/flosch/pongo2"
 	"github.com/julienschmidt/httprouter"
 	"net/http"
 )
 
 func Index(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
-	fmt.Fprintf(w, "hello shuji!")
+	tpl, err := pongo2.FromString("Hello {{ name|capfirst }}!")
+	if err != nil {
+		panic(err)
+	}
+
+	out, err := tpl.Execute(pongo2.Context{"name": "shuji"})
+	if err != nil {
+		panic(err)
+	}
+	fmt.Fprintf(w, out)
 }
