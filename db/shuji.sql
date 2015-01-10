e7a48ee4deadc805205cd5854889204f99a6da67
vimrus <vimrus.com@gmail.com>
2015-01-10 23:38:13 +0800

    + add book table.

--- db/shuji.sql
+++ db/shuji.sql
@@ -1,6 +1,5 @@
 SET NAMES utf8;
 
-DROP TABLE IF EXISTS `user`;
 CREATE TABLE `user` (
       `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
       `name` varchar(30) NOT NULL,
@@ -9,5 +8,14 @@ CREATE TABLE `user` (
       `avatar` varchar(255) NOT NULL,
       `createdat` datetime NOT NULL,
       `lastlogin` datetime NOT NULL,
+      PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+CREATE TABLE `book` (
+      `id` int(11) NOT NULL AUTO_INCREMENT,
+      `name` varchar(100) NOT NULL,
+      `desc` int(11) NOT NULL,
+      `type` varchar(10) NOT NULL,
+      `createdBy` int(11) NOT NULL,
+      `createdAt` datetime NOT NULL,
       PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
