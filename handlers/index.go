87a1fbd93b6655255c7c70cd0aeeca22747cd5f5
vimrus <vimrus.com@gmail.com>
2015-01-19 21:08:38 +0800

    + add article.

--- handlers/index.go
+++ handlers/index.go
@@ -9,12 +9,10 @@ import (
 func Index(c *gin.Context) {
 	session := sessions.Default(c)
 	account := session.Get("account")
-	name := account
-	if name == nil {
+	if account == nil {
 		c.HTML(200, "index.html", pongo2.Context{})
 	} else {
 		c.HTML(200, "home.html", pongo2.Context{
-			"name":    name,
 			"account": account,
 		})
 	}
