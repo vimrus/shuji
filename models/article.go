672e9787ba551f5786f5a1757a784c9c06889118
vimrus <vimrus.com@gmail.com>
2015-01-25 23:35:20 +0800

    + add tinyeditor.

--- models/article.go
+++ models/article.go
@@ -1,1 +1,22 @@
 package models
+
+import (
+	_ "github.com/go-sql-driver/mysql"
+	"time"
+)
+
+type Article struct {
+	Id        int64
+	Title     string
+	Content   string
+	Order     int
+	CreatedBy int64
+	CreatedAt string
+	LastEdit  string
+}
+
+func CreateArticle(title string, userid int64) (*Article, error) {
+	article := &Article{0, title, "", 0, userid, time.Now().Format("2006-01-02 15:04:05"), ""}
+	err := dbmap.Insert(article)
+	return article, err
+}
